db2df6d18b095d0d86d1503206b04b4a5d95093b
kennedybabu <kennedynjogu8@gmail.com>
2021-09-04 19:23:49 +0300

    ask for user input at login

--- run.py
+++ run.py
@@ -11,7 +11,22 @@ def create_user(fname, lname, password):
 
 
 def main():
-    print("Jambo(Hello, in Swahili)!Welcome to Shhh-Locker!")
+    print("Jambo(Hello, in Swahili)!Welcome to Shhh-Locker!.")
+    print("\n")
+
+    user_name = input("Enter your user name: ")
+
+    print(f"What would you like to do? A. Login | B. Create an account")
+
+    short_code = input().lower()
+
+    while True:
+        if short_code == "a":
+            print("Already a User")
+            print("_"*3)
+
+            print("Enter your user name: ")
+            user_Name = input()
 
 
 
--- user.py
+++ user.py
@@ -3,10 +3,19 @@ class User:
      class that generates a new instance of user 
      """
 
+
      user_list = []
 
      def __init__(self, first_name, last_name, password):
-         
-         self.first_name = first_name
-         self.last_name = last_name
-         self.password = password
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+     def save_user(self):
+        """
+        save_user method saves a user object into user_list
+        """
+        User.user_list.append(self)
+
+
